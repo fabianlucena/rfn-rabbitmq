45298a815b913752341975fc6e5554e837cf8e4a
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-08-11 18:57:09 -0300

    First commit.

---
+++ RabbitMQOptions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+
+namespace RFRabbitMQ
+{
+    public class RabbitMQOptions
+    {
+        public string HostName { get; set; } = "localhost";
+        public int Port { get; set; } = 5672;
+        public string UserName { get; set; } = "guest";
+        public string Password { get; set; } = "guest";
+        public string VirtualHost { get; set; } = "/";
+        public SslOption Ssl { get; set; } = new();
+
+        public RabbitMQOptions(IConfiguration configuration)
+        {
+            Configure(configuration);
+        }
+
+        public void Configure(IConfiguration configuration)
+        {
+            HostName = configuration["HostName"] ?? configuration["Hostname"] ?? configuration["Host"] ?? HostName;
+
+            var textPort = configuration["Port"];
+            if (!string.IsNullOrWhiteSpace(textPort) && int.TryParse(textPort, out var port))
+                Port = port;
+
+            UserName = configuration["UserName"] ?? UserName;
+            Password = configuration["Password"] ?? Password;
+            VirtualHost = configuration["VirtualHost"] ?? VirtualHost;
+        }
+    }
+}
---
+++ Reciver.cs
@@ -0,0 +1,73 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RFRabbitMQ
+{
+    public class Reciver
+        : IAsyncDisposable
+    {
+        private RabbitMQOptions Options { get; }
+        private ConnectionFactory ConnectionFactory { get; }
+        public IConnection? Connection { get; private set; }
+        public IChannel? Channel { get; private set; }
+        public string QueueName { get; private set; }
+        public Action<BasicDeliverEventArgs> Handler { get; set;  }
+
+        public Reciver(RabbitMQOptions options, string queueName, Action<BasicDeliverEventArgs> handler)
+        {
+            Options = options;
+            QueueName = queueName;
+            Handler = handler;
+            ConnectionFactory = new ConnectionFactory
+            {
+                HostName = Options.HostName,
+                Port = Options.Port,
+                Ssl = Options.Ssl,
+                UserName = Options.UserName,
+                Password = Options.Password,
+            };
+        }
+
+        public static Reciver Create(RabbitMQOptions options, string queueName, Action<BasicDeliverEventArgs> handler)
+            => new(options, queueName, handler);
+
+        public async Task StartAsync()
+        {
+            Connection = await ConnectionFactory.CreateConnectionAsync();
+            Channel = await Connection.CreateChannelAsync();
+
+            await Channel.QueueDeclareAsync(
+                queue: Options.VirtualHost,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            var consumer = new AsyncEventingBasicConsumer(Channel);
+            consumer.ReceivedAsync += (model, ea) =>
+            {
+                Handler?.Invoke(ea);
+
+                return Task.CompletedTask;
+            };
+
+            await Channel.BasicConsumeAsync(
+                queue: QueueName,
+                autoAck: true,
+                consumer: consumer
+            );
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (Channel is not null)
+                await Channel.CloseAsync();
+
+            if (Connection is not null)
+                await Connection.CloseAsync();
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
